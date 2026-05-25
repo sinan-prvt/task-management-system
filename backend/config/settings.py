e59f2a609f37ce3a10bb84f13e4cc13c58b46a78
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-25 20:15:42 +0530

    Configured PostgreSQL database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management_db',
+        'USER': 'postgres',
+        'PASSWORD': '9400',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
