564b382a55e46e94cd039c274e36827063d1327b
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-26 09:47:34 +0530

    Created Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='Medium')
+    due_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
