a341c6465879f4313c4a224ae9f6738694edc09b
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-26 10:44:32 +0530

    Integrated frontend registration with backend API

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,87 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import API from "../services/api";
+
 function Register() {
-  return <h1>Register Page</h1>;
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      await API.post("/register/", formData);
+
+      alert("Registration Successful");
+
+      navigate("/");
+
+    } catch (error) {
+
+      console.log(error);
+
+      alert("Registration Failed");
+    }
+  };
+
+  return (
+    <div>
+
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Register
+        </button>
+
+      </form>
+
+    </div>
+  );
 }
 
 export default Register;
