12a4e86d163d7be0a1307b647572d92a1c00eea4
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-26 09:59:43 +0530

    Implemented task creation API

---
+++ backend/tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TaskCreateView
+
+
+urlpatterns = [
+    path('tasks/', TaskCreateView.as_view()),
+]
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+
+class TaskCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
