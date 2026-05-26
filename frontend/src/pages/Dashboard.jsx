33b0aa4a3c80b0883bb29b65a72d0a4df77cd8ae
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-26 11:31:23 +0530

    Integrated dashboard with task APIs

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,77 @@
+import { useEffect, useState } from "react";
+
+import API from "../services/api";
+
 function Dashboard() {
 
-  const accessToken = localStorage.getItem("access");
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+
+    try {
+
+      const response = await API.get(
+        "/tasks/list/"
+      );
+
+      setTasks(response.data);
+
+    } catch (error) {
+
+      console.log(error);
+
+      alert("Failed to fetch tasks");
+    }
+  };
+
+  useEffect(() => {
+
+    fetchTasks();
+
+  }, []);
 
   return (
     <div>
 
       <h1>Dashboard</h1>
 
-      <p>User Logged In Successfully</p>
+      <h2>Your Tasks</h2>
 
-      <p>
-        Access Token Exists:
-        {accessToken ? " YES" : " NO"}
-      </p>
+      {
+        tasks.length === 0 ? (
+
+          <p>No Tasks Found</p>
+
+        ) : (
+
+          tasks.map((task) => (
+
+            <div
+              key={task.id}
+              style={{
+                border: "1px solid black",
+                padding: "10px",
+                marginBottom: "10px",
+              }}
+            >
+
+              <h3>{task.title}</h3>
+
+              <p>{task.description}</p>
+
+              <p>
+                Priority: {task.priority}
+              </p>
+
+              <p>
+                Completed:
+                {task.completed ? " Yes" : " No"}
+              </p>
+
+            </div>
+          ))
+        )
+      }
 
     </div>
   );
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+const API = axios.create({
+  baseURL: "http://127.0.0.1:8000/api",
 });
 
-export default api;
+API.interceptors.request.use((config) => {
+
+  const token = localStorage.getItem("access");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default API;
