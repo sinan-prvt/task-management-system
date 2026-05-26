021d3b76664814bc7757e17b7ced7bf9bff48703
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-26 10:54:30 +0530

    Implemented frontend JWT authentication

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,21 @@
 function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+
+  const accessToken = localStorage.getItem("access");
+
+  return (
+    <div>
+
+      <h1>Dashboard</h1>
+
+      <p>User Logged In Successfully</p>
+
+      <p>
+        Access Token Exists:
+        {accessToken ? " YES" : " NO"}
+      </p>
+
+    </div>
+  );
 }
 
 export default Dashboard;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,97 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import API from "../services/api";
+
 function Login() {
-  return <h1>Login Page</h1>;
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      const response = await API.post(
+        "/login/",
+        formData
+      );
+
+      localStorage.setItem(
+        "access",
+        response.data.access
+      );
+
+      localStorage.setItem(
+        "refresh",
+        response.data.refresh
+      );
+
+      alert("Login Successful");
+
+      navigate("/dashboard");
+
+    } catch (error) {
+
+      console.log(error);
+
+      alert("Invalid Credentials");
+    }
+  };
+
+  return (
+    <div>
+
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Login
+        </button>
+
+      </form>
+
+      <br />
+
+      <Link to="/register">
+        Don't have an account?
+      </Link>
+
+    </div>
+  );
 }
 
 export default Login;
