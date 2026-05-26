44a9cd0aa58bbb332ac02ed2c9d2c40febe04e77
sinan-prvt <mohamedsinan9400@gmail.com>
2026-05-26 10:16:15 +0530

    Implemented task update API

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import TaskCreateView, TaskListView
+from .views import (
+    TaskCreateView,
+    TaskListView,
+    TaskUpdateView
+)
 
 
 urlpatterns = [
     path('tasks/', TaskCreateView.as_view()),
     path('tasks/list/', TaskListView.as_view()),
+    path('tasks/<int:pk>/', TaskUpdateView.as_view())
 ]
 
 
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -30,3 +30,34 @@ class TaskListView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+
+class TaskUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk):
+        try:
+            task = Task.objects.get(pk=pk, user=request.user)
+        
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = TaskSerializer(task, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+
+
+
+
+
+
+
